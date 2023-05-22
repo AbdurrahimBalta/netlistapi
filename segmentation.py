cc6167d46e2dba518619ea3608e45544ccee1e26
AbdurrahimBalta <37052634+AbdurrahimBalta@users.noreply.github.com>
2023-05-22 14:41:02 +0300

    aaa
    aa

--- segmentation.py
+++ segmentation.py
@@ -11,18 +11,6 @@ def get_yolov5():
     return model
 
 
-# def get_image_from_bytes(binary_image, max_size=1024):
-#     input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
-#     width, height = input_image.size
-#     resize_factor = min(max_size / width, max_size / height)
-#     resized_image = input_image.resize(
-#         (
-#             int(input_image.width * resize_factor),
-#             int(input_image.height * resize_factor),
-#         )
-#     )
-#     return resized_image
-
 import cv2
 import numpy as np
 
