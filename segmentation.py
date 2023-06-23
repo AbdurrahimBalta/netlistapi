8395ece94baa8b07bcfbed93dff0c1ee81337260
AbdurrahimBalta <37052634+AbdurrahimBalta@users.noreply.github.com>
2023-06-23 17:29:26 +0300

    last baba last
    aa

--- segmentation.py
+++ segmentation.py
@@ -14,11 +14,10 @@ def get_yolov5():
 import cv2
 import numpy as np
 
-def get_image_from_bytes(binary_image, max_size=512):
+
+
+def get_image_from_bytes(binary_image):
     nparr = np.frombuffer(binary_image, np.uint8)
     input_image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    height, width = input_image.shape[:2]
-    resize_factor = min(max_size / width, max_size / height)
-    resized_image = cv2.resize(input_image, None, fx=resize_factor, fy=resize_factor, interpolation=cv2.INTER_AREA)
-    return resized_image
+    return input_image
 
